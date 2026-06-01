cdbf694bf2a68f2092d3252bfd8eb76460b78189
Dave Gabriel <dataresults@outlook.com>
2026-06-01 19:37:31 +0200

    cd wofkflow changes to solve bugs

--- infra/modules/containerization/main.tf
+++ infra/modules/containerization/main.tf
@@ -1,15 +1,33 @@
-resource "azurerm_container_registry" "container_registry" {
+# 1. USER-ASSIGNED MANAGED IDENTITIES
 
+resource "azurerm_user_assigned_identity" "ingestion_identity" {
+  name                = "id-f1-ingestion-${var.resource_name_suffix}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+}
+
+resource "azurerm_user_assigned_identity" "transformation_identity" {
+  name                = "id-f1-transformation-${var.resource_name_suffix}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+}
+
+# 2. SHARED CONTAINER REGISTRY
+
+resource "azurerm_container_registry" "container_registry" {
   name                = "acr${var.resource_name_suffix}"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
   admin_enabled       = true
-
-  tags = var.tags
+  tags                = var.tags
 }
 
-resource "azurerm_container_group" "f1_container_group" {
+# 3. F1 INGESTION CONTAINER GROUP
+
+resource "azurerm_container_group" "f1_ingestion_group" {
   name                = replace("ci-${var.f1_api_ingestion_ci.ci_name}-${var.resource_name_suffix}", "_", "-")
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -17,7 +35,8 @@ resource "azurerm_container_group" "f1_container_group" {
   restart_policy      = "Never"
 
   identity {
-    type = "SystemAssigned"
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.ingestion_identity.id]
   }
 
   container {
@@ -26,7 +45,7 @@ resource "azurerm_container_group" "f1_container_group" {
     cpu    = "0.5"
     memory = "1.0"
 
-  ports {
+    ports {
       port     = 443
       protocol = "TCP"
     }
@@ -47,16 +66,61 @@ resource "azurerm_container_group" "f1_container_group" {
   tags = var.tags
 
   lifecycle {
-    ignore_changes = [
-      container[0].image
-    ]
+    ignore_changes = [container[0].image]
   }
 }
 
+# 4. F1 TRANSFORMATION CONTAINER GROUP
+
+resource "azurerm_container_group" "f1_transformation_group" {
+  name                = replace("ci-${var.f1_transformation_ci.ci_name}-${var.resource_name_suffix}", "_", "-")
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  os_type             = "Linux"
+  restart_policy      = "Never"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.transformation_identity.id]
+  }
+
+  container {
+    name   = replace(var.f1_transformation_ci.ci_name, "_", "-")
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.0"
+
+    environment_variables = {
+      START_SEASON            = var.f1_transformation_ci.start_season
+      END_SEASON              = var.f1_transformation_ci.end_season
+      AZURE_STORAGE_ACCOUNT_URL = "https://${var.storage_account_name}.dfs.core.windows.net"
+    }
+  }
+
+  image_registry_credential {
+    server   = azurerm_container_registry.container_registry.login_server
+    username = azurerm_container_registry.container_registry.admin_username
+    password = azurerm_container_registry.container_registry.admin_password
+  }
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [container[0].image]
+  }
+}
+
+# 5. PERMISSIONS (RBAC) ON STORAGE
+
 data "azurerm_subscription" "current" {}
 
-resource "azurerm_role_assignment" "aci_to_bronze" {
+resource "azurerm_role_assignment" "ingestion_to_storage" {
   scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${var.storage_account_name}"
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_container_group.f1_container_group.identity[0].principal_id
+  principal_id         = azurerm_user_assigned_identity.ingestion_identity.principal_id
+}
+
+resource "azurerm_role_assignment" "transformation_to_storage" {
+  scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${var.storage_account_name}"
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_user_assigned_identity.transformation_identity.principal_id
 }
